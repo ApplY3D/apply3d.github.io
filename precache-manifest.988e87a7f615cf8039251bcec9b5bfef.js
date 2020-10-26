736d25fc1fc26c555041b89dc1039a2c4624a229
Dmitriy Kuzmin <kuzmin_dmitrii@inbox.ru>
2020-10-26 19:22:49 +0200

    Updates

---
+++ precache-manifest.988e87a7f615cf8039251bcec9b5bfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a129ce4e47c1b8cf45d265e6815f1cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62798cbbdc897713a2b5",
+    "url": "/static/css/main.f00f3354.chunk.css"
+  },
+  {
+    "revision": "8e46617059fa9782c498",
+    "url": "/static/js/2.6e439f56.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.6e439f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62798cbbdc897713a2b5",
+    "url": "/static/js/main.aff474f2.chunk.js"
+  },
+  {
+    "revision": "965aca300182a94c14f3",
+    "url": "/static/js/runtime-main.4126d0af.js"
+  }
+]);
